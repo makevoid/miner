b3224d29937c2b6ec68d2cec1bbb6a8beff1c1b0
Francesco Canessa <makevoid@gmail.com>
2014-01-09 04:05:03 +0100

    update cgminer sh

---
+++ cgminer.sh
@@ -0,0 +1,13 @@
+/home/makevoid/apps/cgminer/cgminer -o stratum+tcp://gigahash.wemineltc.com:3335 -u makevoid.mkv4 -p final10 --intensity 11
+
+#API="--api-port 10034 --api-listen --api-allow 0/0"
+#POOL="-o http://ypool.net -u doge1 -p 1"
+#GENERAL="--scrypt --no-submit-stale"
+#CARD="--auto-fan --temp-target 70"
+#WORK="--worksize 256 -I 13"
+#export DISPLAY=:0
+#export GPU_USE_SYNC_OBJECTS=1
+#export GPU_MAX_ALLOC_PERCENT=100
+#cd /home/makevoid/apps/cgminer/
+#./cgminer
+#screen -S miner /home/miner/cgminer/cgminer     
